c02d467c16ad128ae5dbae6fbdfa10f73ffe6771
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-10 21:01:56 -0500

    updated tag.js file

--- E-comm-backend/fantastic-umbrella-main/Develop/models/Tag.js
+++ E-comm-backend/fantastic-umbrella-main/Develop/models/Tag.js
@@ -7,13 +7,22 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
