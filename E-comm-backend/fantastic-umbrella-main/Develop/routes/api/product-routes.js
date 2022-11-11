1fb0617c3b77735193538a88a61b67b3fc53b837
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-10 21:36:01 -0500

    update product routes

--- E-comm-backend/fantastic-umbrella-main/Develop/routes/api/product-routes.js
+++ E-comm-backend/fantastic-umbrella-main/Develop/routes/api/product-routes.js
@@ -7,12 +7,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({ include: [Category, Tag] })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(eer)
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, { include: [Category, Tag] })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(eer);
+    });
 });
 
 // create new product
@@ -91,6 +105,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+   Product.destroy({
+     where: { id: req.params.id },
+   })
+     .then((data) => {
+       res.status(200).json(data);
+     })
+     .catch((err) => {
+       console.log(err);
+     });
 });
 
 module.exports = router;
